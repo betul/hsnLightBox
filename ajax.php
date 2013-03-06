fea62ab467363c3fca4167948510a857402c4b40
Hasan Aydoğdu <hasan_aydogdu@windowslive.com>
2013-03-06 14:19:10 +0200

    Ekleme - Silme - Düzenleme  işlemleri

--- ajax.php
+++ ajax.php
@@ -1,5 +1,9 @@
-﻿<div style="width:300px; height:200px;">
-	<h2>This comes from ajax request</h2>
+﻿<?php 
+$veri = $_POST['veri'];
+?>
+<div style="width:300px; height:200px;">
+	<h2>Ajax dosyasının içeriği</h2>
+    <p>Değişkenimiz : <?=$veri?></p>
 	<p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non velit. Donec pharetra, felis ut tristique adipiscing, diam magna rhoncus neque, sit amet convallis nibh nibh vel libero. Nulla facilisi. In eleifend nisl quis lorem. Duis semper fringilla justo. Proin imperdiet sapien sed lectus. Integer quis nisl et est elementum tempor. Morbi quis tellus nec turpis suscipit molestie. Praesent sed pede. Pellentesque ac orci. Sed sit amet urna eget tellus hendrerit aliquet. Nulla consectetur, pede aliquam ornare placerat, nunc augue commodo leo, sit amet elementum dolor est eleifend magna.
 	</p>
