871badf4ba93e315d52eb6fe3af2927da780e37d
Hasan Aydoğdu <hasan_aydogdu@windowslive.com>
2013-03-13 13:23:52 +0200

    Düzenlemeler yapıldı.
    Beta versiyonu için tüm klasör düzenlendi.

--- ornekler/ajax.php
+++ ornekler/ajax.php
@@ -2,8 +2,8 @@
 $veri = $_POST['veri'];
 ?>
 <div style="width:300px; height:200px;">
-	<h2>Ajax dosyasının içeriği</h2>
-    <p>Değişkenimiz : <?=$veri?></p>
+	<h2>Ajax dosyasinin icerigi</h2>
+    <p>Degiskenimiz : <?=$veri?></p>
 	<p>
     Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean non velit. Donec pharetra, felis ut tristique adipiscing, diam magna rhoncus neque, sit amet convallis nibh nibh vel libero. Nulla facilisi. In eleifend nisl quis lorem. Duis semper fringilla justo. Proin imperdiet sapien sed lectus. Integer quis nisl et est elementum tempor. Morbi quis tellus nec turpis suscipit molestie. Praesent sed pede. Pellentesque ac orci. Sed sit amet urna eget tellus hendrerit aliquet. Nulla consectetur, pede aliquam ornare placerat, nunc augue commodo leo, sit amet elementum dolor est eleifend magna.
 	</p>
